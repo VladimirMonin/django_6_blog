c28dd00d4582548cd9ecd5599c61b68584f8f4b0
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-12-12 19:48:29 +0500

    phase 1.2 feat: Созданы URL-маршруты приложения blog
    - Создан blog/urls.py с тремя паттернами:
      - '' (корень) -> post_list - главная страница
      - 'post/<int:pk>/' -> post_detail - детальный просмотр
      - 'about/' -> about - страница о блоге
    - Настроен app_name = 'blog' для namespace
    - Добавлены комментарии к каждому маршруту

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+"""
+URL-маршруты приложения blog.
+
+Определяет URL-паттерны для просмотра постов и страницы "О блоге".
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # Главная страница - список постов
+    path('', views.post_list, name='post_list'),
+    
+    # Детальный просмотр поста
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    
+    # Страница "О блоге"
+    path('about/', views.about, name='about'),
+]
