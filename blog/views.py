ff5683206ca3b97c300f4b0d9615ef943e84ed74
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-12-12 19:48:08 +0500

    phase 1.2 feat: Созданы view-функции заглушки для проверки маршрутизации
    - Добавлена функция post_list() - главная страница со списком постов
    - Добавлена функция post_detail(pk) - детальный просмотр поста
    - Добавлена функция about() - страница 'О блоге'
    - Все функции возвращают HttpResponse с HTML для временной проверки
    - Добавлены docstring для каждой функции согласно гайдлайнам

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def post_list(request):
+    """
+    Список постов блога (главная страница).
+    
+    Временная заглушка для проверки маршрутизации.
+    """
+    return HttpResponse("<h1>Список постов</h1><p>Здесь будет список всех постов блога.</p>")
+
+
+def post_detail(request, pk):
+    """
+    Детальный просмотр поста.
+    
+    Args:
+        pk: Primary key поста
+    
+    Временная заглушка для проверки маршрутизации.
+    """
+    return HttpResponse(f"<h1>Пост #{pk}</h1><p>Здесь будет детальный просмотр поста с ID {pk}.</p>")
+
+
+def about(request):
+    """
+    Страница "О блоге".
+    
+    Временная заглушка для проверки маршрутизации и навигации.
+    """
+    return HttpResponse("<h1>О блоге</h1><p>Информация о блоге на Django 6 + HTMX + Bootstrap 5.</p>")
+
