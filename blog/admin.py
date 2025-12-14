46103fb3a61d9208fb9d6ca87788fd6525a1bc0a
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-12-14 17:57:46 +0500

    formatter:

--- blog/admin.py
+++ blog/admin.py
@@ -7,25 +7,18 @@ class PostAdmin(admin.ModelAdmin):
     """
     Административный интерфейс для модели Post.
     """
-    list_display = ('title', 'slug', 'created_at', 'is_published')
-    list_filter = ('is_published', 'created_at')
-    search_fields = ('title', 'content')
-    prepopulated_fields = {'slug': ('title',)}
-    date_hierarchy = 'created_at'
-    ordering = ('-created_at',)
-    
-    fieldsets = (
-        ('Основная информация', {
-            'fields': ('title', 'slug', 'content')
-        }),
-        ('Статус', {
-            'fields': ('is_published',)
-        }),
-        ('Даты', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
-    
-    readonly_fields = ('created_at', 'updated_at')
 
+    list_display = ("title", "slug", "created_at", "is_published")
+    list_filter = ("is_published", "created_at")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+    date_hierarchy = "created_at"
+    ordering = ("-created_at",)
+
+    fieldsets = (
+        ("Основная информация", {"fields": ("title", "slug", "content")}),
+        ("Статус", {"fields": ("is_published",)}),
+        ("Даты", {"fields": ("created_at", "updated_at"), "classes": ("collapse",)}),
+    )
+
+    readonly_fields = ("created_at", "updated_at")
